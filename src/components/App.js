83f65b89462d9eb4eb430a040979f1bbc4e898a5
reyraditya <reyhanraditya@icloud.com>
2019-07-04 14:18:51 +0700

    register

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import Login from "./Login";
 import Home from "./Home";
+import Register from "./Register"
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </BrowserRouter>
     );
   }
